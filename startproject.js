377f1d06d16f4aaabc730434dcb31daae9c9a4ec
aeyler <42477570+aeyler@users.noreply.github.com>
2019-10-07 11:02:34 -0500

    Selector updates
    Able to select project and category and updat the button with text and value attribute.
    Also added TimeData struct (as I got ahead of myself) but no functionality.

--- startproject.js
+++ startproject.js
@@ -2,13 +2,20 @@
 
 var USETESTDATA = true;
 
+function TimeData (projectId, categoryId, description, startTime) {
+    this.projectId = projectId;
+    this.categoryId = categoryId;
+    this.description = description;
+    this.startTime = startTime;
+}
+
 // Fill out the project and category selections
 function performLoadOperations() {
     var projectList = retrieveProjectList(USETESTDATA);
     var categoryList = retrieveCategoryList(USETESTDATA);
 
     console.log(JSON.stringify(projectList));
-    
+
     document.getElementById("time_debugDisplayProjectJson").textContent = JSON.stringify(projectList);
     document.getElementById("time_debugDisplayCategoryJson").textContent = JSON.stringify(categoryList);
 
@@ -22,9 +29,8 @@ function addProjectsToDropdown(projectList) {
         alert("No Projects Defined");
         return;
     }
-    var projectDropdown = document.getElementById("time_selectProjectDropdown");
     for (var i = 0; i < projectList.length; i++) {
-        addElementToDropdown(projectDropdown, projectList[i].projectName);
+        addProjectDataToDropdown(projectList[i]);
     }
 }
 
@@ -34,15 +40,41 @@ function addCategoriesToDropdown(categoryList) {
         alert("No Categories Defined");
         return;
     }
-    var categoryDropdown = document.getElementById("time_selectCategoryDropdown");
     for (var i = 0; i < categoryList.length; i++) {
-        addElementToDropdown(categoryDropdown, categoryList[i].categoryName);
+        addCategoryDataToDropdown(categoryList[i]);
     }
 }
 
-function addElementToDropdown(parent, elementSting) {
-    var p = document.createElement("div");
-    p.className = "w3-bar-item w3-button"; // denote a button(esque) look/feel
-    p.textContent = elementSting;
-    parent.appendChild(p);
+function getDivItem(name, id) {
+    var div = document.createElement("div");
+    div.className = "w3-bar-item w3-button"; // denote a button(esque) look/feel
+    div.textContent = name;
+    div.setAttribute("value", id);
+    return div;
+}
+
+function addProjectDataToDropdown(projectData) {
+    var div = getDivItem(projectData.projectName, projectData.projectId);
+
+    div.addEventListener("click", function () {
+        var button = document.getElementById("time_selectProjectButton");
+        button.textContent = projectData.projectName;
+        button.setAttribute("value", projectData.projectId);
+    });
+
+    var projectDropdown = document.getElementById("time_selectProjectDropdown");
+    projectDropdown.appendChild(div);
+}
+
+function addCategoryDataToDropdown(categoryData) {
+    var div = getDivItem(categoryData.categoryName, categoryData.categoryId);
+
+    div.addEventListener("click", function () {
+        var button = document.getElementById("time_selectCategoryButton");
+        button.textContent = categoryData.categoryName;
+        button.setAttribute("value", categoryData.categoryId);
+    });
+
+    var categoryDropdown = document.getElementById("time_selectCategoryDropdown");
+    categoryDropdown.appendChild(div);
 }
