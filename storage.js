993ef05205914cd66bfa1f99b6f58fc8c515f937
aeyler <42477570+aeyler@users.noreply.github.com>
2019-10-07 15:43:15 -0500

    Update storage.js
    Adding test timeDatatList

--- storage.js
+++ storage.js
@@ -1,7 +1,8 @@
 "use strict";
 
-var test_ProjectList = '[{"projectName":"test proj 1","projectId":"11111"},{"projectName":"test project 22","projectId":"22222"},{"projectName":"test project 333","projectId":"33333"}]';
-var test_CategoryList = '[{"categoryName":"test cat 1","categoryId":"test cat 1"},{"categoryName":"test cat 22","categoryId":"test cat 22"}]';
+var test_ProjectList = '[{"projectName":"test proj 1","projectId":"11111"},{"projectName":"test project 22","projectId":"22222"},{"projectName":"test project 333","projectId":"33333"},{"projectName":"no proj","projectId":"no proj"}]';
+var test_CategoryList = '[{"categoryName":"test cat 1","categoryId":"test cat 1"},{"categoryName":"test cat 22","categoryId":"test cat 22"},{"categoryName":"no cat","categoryId":"no cat"}]';
+var test_TimeDataList = '[{"projectData":{"projectName":"test proj 1","projectId":"11111"},"categoryData":{"categoryName":"test cat 1","categoryId":"test cat 1"},"description":"asdf","startTimeDateString":"Mon Oct 07 2019 08:00:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"test proj 1","projectId":"11111"},"categoryData":{"categoryName":"test cat 1","categoryId":"test cat 1"},"description":"","startTimeDateString":"Mon Oct 07 2019 10:00:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"test proj 22","projectId":"22222"},"categoryData":{"categoryName":"test cat 22","categoryId":"test cat 22"},"description":"","startTimeDateString":"Mon Oct 07 2019 11:00:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"no proj","projectId":"no proj"},"categoryData":{"categoryName":"no cat","categoryId":"no cat"},"description":"out to launch :)","startTimeDateString":"Mon Oct 07 2019 11:30:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"test proj 333","projectId":"33333"},"categoryData":{"categoryName":"test cat 1","categoryId":"test cat 1"},"description":"Back to cat 1, huh?","startTimeDateString":"Mon Oct 07 2019 13:00:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"test proj 1","projectId":"11111"},"categoryData":{"categoryName":"test cat 1","categoryId":"test cat 1"},"description":"late in the day thing...","startTimeDateString":"Mon Oct 07 2019 16:30:00 GMT-0500 (Central Daylight Time)"},{"projectData":{"projectName":"no proj","projectId":"no proj"},"categoryData":{"categoryName":"no cat","categoryId":"no cat"},"description":"done for the day","startTimeDateString":"Mon Oct 07 2019 17:30:00 GMT-0500 (Central Daylight Time)"}]';
 
 function debug_clearLocalStorage() {
     localStorage.clear();
@@ -74,9 +75,9 @@ function storeTimeDataList(timeDataList) {
 // Return: TimeDataList data from localStorage
 function retrieveTimeDataList(requestTestData) {
     var jsonList = localStorage.getItem("timeDataList");
-    // if (requestTestData === true) {
-    //     jsonList = test_ProjectList;
-    // }
+    if (requestTestData === true) {
+        jsonList = test_TimeDataList;
+    }
     var programList = JSON.parse(jsonList);
     return programList;
 }
