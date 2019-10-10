e7d98c1f38a4c78c704209ce437be78fd988c476
aeyler <42477570+aeyler@users.noreply.github.com>
2019-10-10 08:55:12 -0500

    Added display of NI test data in debug string

--- testdata.js
+++ testdata.js
@@ -21,19 +21,25 @@ function onButtonClick_testdata_clearLocalStorage() {
 function onButtonClick_testdata_insertTestProjectData() {
     console.log("Inserting test project data");
     storeProjectListJsonString(test_ProjectListAsJsonString);
-    document.getElementById("test_dataInsertedToLocalStorage").textContent = test_ProjectListAsJsonString;
+    var debugDisplayString = "<b>Project List</b><br>";
+    debugDisplayString += test_ProjectListAsJsonString;
+    document.getElementById("test_dataInsertedToLocalStorage").innerHTML = debugDisplayString;
 }
 
 function onButtonClick_testdata_insertTestCategoryData() {
     console.log("Inserting test category data");
     storeCategoryListJsonString(test_CategoryListAsJsonString);
-    document.getElementById("test_dataInsertedToLocalStorage").textContent = test_CategoryListAsJsonString;
+    var debugDisplayString = "<b>Category List</b><br>";
+    debugDisplayString += test_CategoryListAsJsonString;
+    document.getElementById("test_dataInsertedToLocalStorage").innerHTML = debugDisplayString;
 }
 
 function onButtonClick_testdata_insertTestTimeEntryData() {
     console.log("Inserting test time entry data");
     storeTimeEntryListJsonString(test_TimeEntryListAsJsonString);
-    document.getElementById("test_dataInsertedToLocalStorage").textContent = test_TimeEntryListAsJsonString;
+    var debugDisplayString = "<b>Time Entry List</b><br>";
+    debugDisplayString += test_TimeEntryListAsJsonString;
+    document.getElementById("test_dataInsertedToLocalStorage").innerHTML = debugDisplayString;
 }
 
 function onButtonClick_testdata_gimmeNITestData() {
@@ -42,7 +48,13 @@ function onButtonClick_testdata_gimmeNITestData() {
     storeProjectListJsonString(test_NIProjectList);
     storeCategoryListJsonString(test_NICategoryList);
     storeTimeEntryListJsonString(test_NITimeEntryList);
-    document.getElementById("test_dataInsertedToLocalStorage").innerHTML = "<i><b>...Way to much data to show, so trust me you fool...</b></i>";
+    var debugDisplayString = "<b>Project List</b><br>";
+    debugDisplayString += test_NIProjectList;
+    debugDisplayString += "<br><b>Category List</b><br>";
+    debugDisplayString += test_NICategoryList;
+    debugDisplayString += "<br><b>Time Entry List</b><br>";
+    debugDisplayString += test_NITimeEntryList;
+    document.getElementById("test_dataInsertedToLocalStorage").innerHTML = debugDisplayString;
 }
 
 function onButtonClick_testData_enableDebugAreas() {
